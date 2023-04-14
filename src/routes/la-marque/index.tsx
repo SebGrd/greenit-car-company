a2e55422fdb59fb6353819ecb8ac3fd63cfbc8aa
Chaochao-z <chaochao.zhou@hotmail.com>
2023-04-14 15:43:15 +0200

    Page La marque

--- src/routes/la-marque/index.tsx
+++ src/routes/la-marque/index.tsx
@@ -5,7 +5,20 @@ export default component$(() => {
   return (
     <>
     <section>
-      Brand
+      <h1 class="text-3xl md:text-4xl font-bold mb-12">La Marque OPTA</h1>
+    </section>
+    <section class="xl:flex flex-column mb-24">
+      <div class="2xl:w-1/2 xl:w-3/6 w-full bg-gray-300 px-10 py-5">
+        <h1 class="text-4xl md:text-5xl font-bold mb-12 mt-5">Notre <br />Histoire</h1>
+        <p class="text-lg">Le changement climatique et les pressions réglementaires pour réduire les émissions de gaz à effet de serre ont également stimulé l'intérêt pour les voitures électriques. De nombreux pays ont adopté des politiques pour encourager l'achat de voitures électriques, telles que des subventions ou des incitations fiscales.
+          Aujourd'hui, les voitures électriques sont de plus en plus populaires, et de nombreux constructeurs automobiles proposent désormais des modèles électriques pour répondre à la demande croissante.</p>
+
+      </div>
+      <picture class="2xl:w-1/2 xl:w-2/3 w-full">
+        <source srcSet="/img/rechauffement.webp" type="image/webp" />
+        <source srcSet="/img/rechauffement.avif" type="image/avif" />
+        <img src="/img/rechauffement.jpg" alt="OPTA UV3" loading="lazy" />
+      </picture>
     </section>
     </>
   );
