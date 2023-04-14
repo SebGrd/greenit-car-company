6894e8d653d514a72bfb3f43d0eb45656c540d9c
Seb <sebastien.gaudard.1@gmail.com>
2023-04-14 04:32:44 +0200

    Feat: Added routes

---
+++ src/routes/modeles/uv3/index.tsx
@@ -0,0 +1,22 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <>
+    <section>
+      UV3
+    </section>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'OPTA - Eco friendly cars',
+  meta: [
+    {
+      name: 'description',
+      content: 'We make the best eco friendly cars in the world. From small cars to big SUVs, we are changing the automotive industry.',
+    },
+  ],
+};
